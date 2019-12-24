a5d7430a74bad4a996176d4f142a62ff9688214d
Hoda <hodiharati@gmail.com>
2019-12-24 19:54:46 +0330

    define slider

--- app/src/main/java/com/example/commerce/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/commerce/adapter/ProductAdapter.java
@@ -1,4 +1,34 @@
 package com.example.commerce.adapter;
 
-public class ProductAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductHolder> {
+
+
+    @NonNull
+    @Override
+    public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ProductHolder extends RecyclerView.ViewHolder{
+
+        public ProductHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/commerce/view/CommerceFragment.java
+++ app/src/main/java/com/example/commerce/view/CommerceFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,13 +14,21 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.example.commerce.R;
+import com.example.commerce.databinding.CommerceFragmentBinding;
+
+import java.util.HashMap;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CommerceFragment extends Fragment {
+
+    CommerceFragmentBinding mBinding;
 
     public static CommerceFragment newInstance() {
         
@@ -46,7 +55,23 @@ public class CommerceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.commerce_fragment, container, false);
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.commerce_fragment, container, false);
+        mBinding.executePendingBindings();
+
+        setUpSlider();
+        return mBinding.getRoot();
+    }
+
+    private void setUpSlider(){
+        HashMap<String, Integer> images = new HashMap<>();
+        images.put("image1",R.drawable.image1);
+        images.put("image2", R.drawable.image2);
+        images.put("image3", R.drawable.image3);
+        for (String imageName: images.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            textSliderView.description(imageName).image(images.get(imageName)).setScaleType(BaseSliderView.ScaleType.CenterCrop);
+            mBinding.sliderLayout.addSlider(textSliderView);
+        }
     }
 
     @Override
